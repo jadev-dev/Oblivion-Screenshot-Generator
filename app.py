8b48a80da95ac431bd186b02735540e0b0ce831c
jadev-dev <me@jadev.dev>
2022-01-30 09:40:22 +0000

    bug fixes for chrome and mobile safari

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from generator import make_image
 APP = Flask(__name__, template_folder="./www")
 
 
-@APP.route("/", methods=['POST', 'GET'])
+@APP.route("/oblivion.html", methods=['POST', 'GET'])
 def input_page():
     npc_list = [file for file in sorted(listdir("./faces")) if not file[0] == "."]
     if request.method == 'POST':
@@ -19,7 +19,7 @@ def input_page():
     else:
         return render_template("generator.html", encoded_image=None, npc_list=npc_list)
 
-@APP.route("/style.css", methods=['GET'])
+@APP.route("/oblivion/style.css", methods=['GET'])
 def style_sheet():
     pan_directions = [["left", "right"], ["right", "left"]]
     y_position = random.randint(25, 75)
